6247bd35afd37d42d8cef61df039c28c3cadaff2
Luís Gabriel Nascimento Simas <luis.gabriel_thera@prestador.globo>
2024-04-24 09:25:25 -0300

    add: TODO for OOP Convert later

--- #1-HelloWorld/hello_world.py
+++ #1-HelloWorld/hello_world.py
@@ -1,6 +1,4 @@
 """_summary_
-*Summary*
-
 **Exercise Description**
 
 Write a program that, when run, greets the user by printing “Hello, world!” on the screen. Then it prints a message on the screen asking the user to enter their name. The program greets the user by name by printing the “Hello,” followed by the user’s name.
@@ -14,7 +12,7 @@ Alice
 Hello, Alice
 
 """
-
+# TODO Convert to OOP
 print("Hello, world!")
 print("What is your name? ")
 name = input()
